25a44b507eb4dcb0d000ac1d8426db09cdf6690f
cedric kadima <kadimacedric3@gmail.com>
2024-06-24 15:31:09 +0200

    ajout de la connexion a ala base de données

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./db');
+
+// Charger les variables d'environnement
+dotenv.config();
+
+// Connecter à MongoDB
+connectDB();
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
